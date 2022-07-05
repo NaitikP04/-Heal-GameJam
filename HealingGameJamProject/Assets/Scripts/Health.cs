e29f2ffe9278b740cab1f11ed5caab4304aa5cc5
Naitik Poddar <poddarnaitik58@gmail.com>
2022-07-05 23:43:46 +0530

    Added map png to sprites folder, removed half hearts

--- HealingGameJamProject/Assets/Scripts/Health.cs
+++ HealingGameJamProject/Assets/Scripts/Health.cs
@@ -12,7 +12,6 @@ public class Health : MonoBehaviour
     public Image[] hearts;
     public Sprite fullHeart;
     public Sprite emptyHeart;
-    public Sprite halfHeart;
 
     // The amount of time after getting hurt that we're immune to damage.
     [SerializeField] float invulnerableTime = 0.5f;
@@ -26,13 +25,6 @@ public class Health : MonoBehaviour
         for(int i = 0; i<hearts.Length; i ++){  //for each heart in the array
             if(i < health){
                 hearts[i].sprite = fullHeart;
-            }
-            // implement a system to show halfHeart when health decreases by 0.5 hearts
-            // this always displays an additional half heart, doesnt work properly when health is incremented by 0.5
-            // find a fix for this
-
-            else if(i < health + 0.5f){ 
-                hearts[i].sprite = halfHeart;
             }
             else{
                 hearts[i].sprite = emptyHeart;
