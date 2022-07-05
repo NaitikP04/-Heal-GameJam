e541c30d4a6f81f0815b525f75166323d659d9df
Dylan / Marigold <62458416+gold-mari@users.noreply.github.com>
2022-07-05 09:11:24 -0700

    Update EnemyChase.cs
    - Enemy sprite now flips to match direction

--- HealingGameJamProject/Assets/Scripts/EnemyChase.cs
+++ HealingGameJamProject/Assets/Scripts/EnemyChase.cs
@@ -9,19 +9,28 @@ public class EnemyChase : MonoBehaviour
 
     Rigidbody2D rgbd2d;
     Component[] childTransforms;
+    SpriteRenderer sprite;
 
     private void Awake()
     {
         rgbd2d = GetComponent<Rigidbody2D>();
         childTransforms = GetComponentsInChildren<Transform>();
+        sprite = GetComponent<SpriteRenderer>();
         //targetGameobject = targetDestination.gameObject;
     }
 
     private void FixedUpdate()
     {
+        // Move self...
         Vector3 direction = (targetDestination.position - transform.position).normalized;
         rgbd2d.velocity = direction.normalized * speed;
+        // And move all children.
         foreach (Transform trans in childTransforms){ trans.position = this.transform.position; }
+
+        // SPRITE FLIPPING
+        if (direction.x > 0){ sprite.flipX = true; }
+        if (direction.x < 0){ sprite.flipX = false; }
+
     }
 
 }
