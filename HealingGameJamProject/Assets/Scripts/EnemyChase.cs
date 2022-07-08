2f9f22ba2eb21c96189258cadc229e3d5f82198d
Dylan / Marigold <62458416+gold-mari@users.noreply.github.com>
2022-07-07 23:51:49 -0700

    Spiced up zombie movement
    1) Zombies now have a chance to stop randomly. (With idle animation!)
    2) Zombies now have a chance to move in a random direction for a while.

--- HealingGameJamProject/Assets/Scripts/EnemyChase.cs
+++ HealingGameJamProject/Assets/Scripts/EnemyChase.cs
@@ -10,7 +10,15 @@ public class EnemyChase : MonoBehaviour
     Rigidbody2D rgbd2d;
     Component[] childTransforms;
     SpriteRenderer sprite;
-    public bool stopped = false;
+    Animator zombAnimator;
+
+    [HideInInspector] public bool stopped = false;
+
+    [Header("=== PARAMETERS ===")]
+    public bool stopRandomly = true;
+    public bool moveRandomly = true;
+    bool randomNow;
+    Vector3 randomDirection = new Vector3();
 
     private void Awake()
     {
@@ -18,7 +26,14 @@ public class EnemyChase : MonoBehaviour
         rgbd2d = GetComponent<Rigidbody2D>();
         childTransforms = GetComponentsInChildren<Transform>();
         sprite = GetComponent<SpriteRenderer>();
+        zombAnimator = GetComponent<Animator>();
         //targetGameobject = targetDestination.gameObject;
+    }
+
+    private void Start()
+    {
+        if (stopRandomly) {StartCoroutine(RandomStops());}
+        if (moveRandomly) {StartCoroutine(ReadRandomMovements());}
     }
 
     private void FixedUpdate()
@@ -26,7 +41,12 @@ public class EnemyChase : MonoBehaviour
         if (!stopped)
         {
             // Move self...
-            Vector3 direction = (targetDestination.position - transform.position).normalized;
+            Vector3 direction;
+            // If we're not currently moving in a random direction, move towards the target.
+            if (!randomNow) { direction = (targetDestination.position - transform.position).normalized; }
+            // Otherwise, move in the random direction as determined by ReadRandomMovements().
+            else { direction = randomDirection.normalized;}
+
             rgbd2d.velocity = direction.normalized * speed;
             // And move all children.
             foreach (Transform trans in childTransforms){ trans.position = this.transform.position; }
@@ -35,6 +55,40 @@ public class EnemyChase : MonoBehaviour
             if (direction.x > 0){ sprite.flipX = false; }
             if (direction.x < 0){ sprite.flipX = true; }
         }
-        else { rgbd2d.velocity = Vector3.zero; }
+        else
+        {
+            rgbd2d.velocity = Vector3.zero;
+        }
+
+        zombAnimator.SetBool("isMoving", stopped);
+        
     }
+
+    IEnumerator RandomStops()
+    {
+        while (true)
+        {
+            if (Random.Range(1,7) == 4){
+                stopped = true;
+                yield return new WaitForSeconds(1);
+                stopped = false;
+            }
+            else { yield return new WaitForSeconds(3); }
+        }  
+    }
+
+    IEnumerator ReadRandomMovements()
+    {
+        while (true)
+        {
+            if (Random.Range(1,4) == 3){
+                randomNow = true;
+                randomDirection = new Vector3(Random.Range(0f,1f),Random.Range(0f,1f),0f);
+                yield return new WaitForSeconds(1.5f);
+                randomNow = false;
+            }
+            else { yield return new WaitForSeconds(5); }
+        }  
+    }
+
 }
