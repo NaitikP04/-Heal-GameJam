a24155081c85e62b2f3df28461ec42a95146dd7e
Naitik Poddar <poddarnaitik58@gmail.com>
2022-07-16 00:19:52 +0530

    Implemented waves and wave controller

--- HealingGameJamProject/Assets/Scripts/DEBUGZombSpawner.cs
+++ HealingGameJamProject/Assets/Scripts/DEBUGZombSpawner.cs
@@ -6,25 +6,115 @@ public class DEBUGZombSpawner : MonoBehaviour
 {
 
     // Donut-based spawn range from: https://answers.unity.com/questions/1580130/i-need-to-instantiate-an-object-inside-a-donut-ins.html
+    // SHOUTOUT TO BRACKEYS!!!!
 
     [SerializeField] GameObject zomb;
     public float innerRadius = 10f;
     public float outerRadius = 20f;
-    public float delay = 2f;
 
-    // Start is called before the first frame update
-    void Start()
+    public enum SpawnState {SPAWNING, WAITING, COUNTING};
+    
+    [System.Serializable]
+    public class Wave
     {
-        StartCoroutine(SpawnRoutine());
+        public string name;
+        public int count;
+        public float rate;
     }
 
-    IEnumerator SpawnRoutine()
+    public Wave[] waves;
+    private int nextWave = 0;
+
+    public float timeBetweenWaves = 5f;
+    public float waveCountdown;
+
+    private float searchCountdown = 1f;
+
+    public SpawnState state = SpawnState.COUNTING;
+
+    void Start()
     {
-        while (true)
+        waveCountdown = timeBetweenWaves;
+        //StartCoroutine(SpawnRoutine());
+    }
+
+    void Update()
+    {
+        if (state == SpawnState.WAITING)
         {
-            Instantiate(zomb, GetSpawnPoint(), Quaternion.identity, this.transform);
-            yield return new WaitForSeconds(delay);
+            if (!EnemyIsAlive())
+            {
+                WaveCompleted();
+            }
+            else
+            {
+                return;
+            }
         }
+        
+        if(waveCountdown <= 0)
+        {
+            if(state != SpawnState.SPAWNING)
+            {
+                StartCoroutine(SpawnRoutine(waves[nextWave]));
+            }
+        }
+        else
+        {
+            waveCountdown -= Time.deltaTime;
+        }
+    }
+
+    void WaveCompleted()
+    {
+        Debug.Log("Wave Completed");
+        
+        state = SpawnState.COUNTING;
+        waveCountdown = timeBetweenWaves;
+
+        if(nextWave + 1 > waves.Length - 1)
+        {
+            nextWave = 0;
+            Debug.Log("Completed all waves! Looping..."); //change to a game completed screen
+        }
+        else
+        {
+            nextWave++;
+        }
+    }
+    
+    bool EnemyIsAlive()
+    {
+        searchCountdown-=Time.deltaTime;
+
+        if(searchCountdown <=0f)
+        {
+            searchCountdown = 1f;
+            if (GameObject.FindGameObjectWithTag("Enemy") == null)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    IEnumerator SpawnRoutine(Wave _wave)
+    {
+        state = SpawnState.SPAWNING;
+
+        //spawn
+        for(int i = 0; i < _wave.count; i++)
+        {
+            SpawnZomb();
+            yield return new WaitForSeconds(1f / _wave.rate);
+        }
+
+        state = SpawnState.WAITING;
+    }
+
+    void SpawnZomb()
+    {
+        Instantiate(zomb, GetSpawnPoint(), Quaternion.identity, this.transform);
     }
 
     Vector3 GetSpawnPoint()
