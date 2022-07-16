6829da05c4889395236f9a07e954fcfb0414ec5c
Dylan / Marigold <62458416+gold-mari@users.noreply.github.com>
2022-07-15 17:04:23 -0700

    Powerups now passively emit particles
    - Particles are offwhite, used to draw the player's attention to them

---
+++ HealingGameJamProject/Assets/Scripts/PassiveParticles.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PassiveParticles : MonoBehaviour
+{
+    [SerializeField] GameObject particle;
+    [SerializeField] Color particleColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnParticles());
+    }
+
+    IEnumerator SpawnParticles()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(3,6));
+
+            for (int i = 0; i < 4; i++){
+                    GameObject p = Instantiate(particle, this.transform.position, Quaternion.identity, this.transform.parent.transform);
+                    p.GetComponent<ParticleBehavior>().color = particleColor;
+                    print("PARTICLE HAS SPAWNED");
+                }
+        }
+    }
+}
